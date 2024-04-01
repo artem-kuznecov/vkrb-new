d36765a2030ef77a84f2e360fa58bc0ab51a0e36
artem-kuznecov <artemkuz.wmg@gmail.xn--co-nnc>
2024-04-01 23:00:24 +0300

    Setting structure & adding icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path'
+import { URL } from 'url'
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+const nextConfig = {
+  distDir: 'dist',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/theme')],
+    prependData: `
+      @import "_variables.scss";
+      @import "_classes.scss";
+    `
+  }
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from 'next'
+import { Geologica } from 'next/font/google'
+
 import './globals.scss'
 
 export const metadata: Metadata = {
   title: 'LISA'
 }
 
+const geologica = Geologica({ subsets: ['cyrillic', 'latin'] })
+
 export default function RootLayout ({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
     <html suppressHydrationWarning>
-      <body>{children}</body>
+      <body className={geologica.className}>{children}</body>
     </html>
   )
 }
